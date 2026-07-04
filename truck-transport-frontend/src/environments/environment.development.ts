3734784d9cde56323e12ce746aabd65474ee8805
Yashwanth1129 <ananthayashwanth143@gmail.com>
2026-07-04 18:31:58 -0400

    change made to environment

--- truck-transport-frontend/src/environments/environment.development.ts
+++ truck-transport-frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://localhost:7001/api',
   assets: {
     logo: '/newlogo.png',
     loginBackground: '/background.jpeg',
